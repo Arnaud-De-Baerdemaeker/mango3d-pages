007c7a89cd34ee3557bbf2783f3d517c82cb80a7
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 17:01:15 +0100

    Add styling
    Imported styles from the SCSS file.
    Added the className to the tags.

--- pages/components/overview-page/for-all-industries-components/dark-figurines.js
+++ pages/components/overview-page/for-all-industries-components/dark-figurines.js
@@ -1,26 +1,31 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../../scss-components/models.module.scss";
+
 function DarkFigurines() {
     return (
-        <div>
-            <figure>
+        <>
+            <figure className={styles.models__imageContainer}>
                 <img
                     src={"/lychee-slicer-dark-figurines.jpg"}
                     title={"lychee-slicer-dark-figurines"}
+                    className={styles.models__image}
                 />
-                <figcaption>
-                    {"3D model:"}
+                <figcaption className={styles.models__imageCaption}>
+                    {"3D model: "}
                     <Link
                         href={
                             "https://www.myminifactory.com/users/DarkFigurines"
                         }>
-                        <a>{"Dark Figurines"}</a>
+                        <a className={styles.models__imageLink}>
+                            {"Dark Figurines"}
+                        </a>
                     </Link>
                 </figcaption>
             </figure>
-            <h3>{"Figurines and Minis"}</h3>
-        </div>
+            <h3 className={styles.models__title}>{"Figurines and Minis"}</h3>
+        </>
     );
 }
 
