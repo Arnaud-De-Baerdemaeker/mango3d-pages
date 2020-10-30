77ef87c66b2537e4b3bb20bee14bd8544173f9b8
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 13:29:33 +0100

    Add classNames and replace a <div> by <figure>

--- pages/components/overview-page/title.js
+++ pages/components/overview-page/title.js
@@ -1,33 +1,41 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../scss-components/title.module.scss";
+
 function Title() {
     return (
-        <div>
-            <div>
-                <div>
+        <div className={styles.title__container}>
+            <div className={styles.title__subContainer}>
+                <figure className={styles.title__logo}>
                     <img src={"/lychee_small.png"} />
+                </figure>
+                <div className={styles.title__mainTitle}>
+                    <h1 className={styles.title__mainTitle}>
+                        {"Lychee Slicer 3"}
+                    </h1>
                 </div>
                 <div>
-                    <h1>{"Lychee Slicer 3"}</h1>
-                </div>
-                <div>
-                    <h2>
+                    <h2 className={styles.title__subTitle}>
                         {
                             "A powerful and versatile slicer for SLA resin 3D printers"
                         }
                     </h2>
                 </div>
             </div>
-            <div>
-                <div>
+            <div className={styles.title__links}>
+                <div className={styles.title__button}>
                     <Link href={"/downloads"}>
-                        <a>{"Get Lychee Slicer 3 Free Edition"}</a>
+                        <a className={styles.title__link}>
+                            {"Get Lychee Slicer 3 Free Edition"}
+                        </a>
                     </Link>
                 </div>
-                <div>
+                <div className={styles.title__button}>
                     <Link href={"#"}>
-                        <a>{"Compare versions"}</a>
+                        <a className={styles.title__link}>
+                            {"Compare versions"}
+                        </a>
                     </Link>
                 </div>
             </div>
