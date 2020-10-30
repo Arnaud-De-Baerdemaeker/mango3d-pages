c13321363ff98df3f832426b6fc108e0960001e7
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 17:00:13 +0100

    Add styling
    Imported styles from the SCSS file.
    Added classNames to tags.

--- pages/components/overview-page/for-all-industries-components/cad-model.js
+++ pages/components/overview-page/for-all-industries-components/cad-model.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import styles from "./../../scss-components/models.module.scss";
+
 function CadModel() {
     return (
-        <div>
-            <figure>
+        <>
+            <figure className={styles.models__imageContainer}>
                 <img
                     src={"/lychee-slicer-cad-3d-model.jpg"}
                     title={"lychee-slicer-cad-3d-model"}
+                    className={styles.models__image}
                 />
             </figure>
-            <h3>{"Product Design / Makers"}</h3>
-        </div>
+            <h3 className={styles.models__title}>
+                {"Product Design / Makers"}
+            </h3>
+        </>
     );
 }
 
