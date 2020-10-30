123a27a1f6aaac59f4835e5ed09c4cb43ef149bb
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 13:30:19 +0100

    Add a className to the main tag

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import CompatibilityList from "./components/overview-page/compatibility-list";
 import Footer from "./components/general-components/footer";
 import BackToTop from "./components/general-components/back-to-top";
 
-// import styles from "./components/scss-components/index.module.scss";
+import styles from "./components/scss-components/index.module.scss";
 
 function LycheeSlicer() {
     return (
@@ -18,7 +18,7 @@ function LycheeSlicer() {
                 {"*{ box-sizing: border-box; } body{ margin: 0px; }"}
             </style>
             <Header />
-            <main>
+            <main className={styles.mainContainer}>
                 <Title />
                 <Intro />
                 <ForAllIndustries />
