cb99dd6dd6c3609b628f468400246f2f6254b908
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 15:13:46 +0100

    Set the dividing waves

---
+++ pages/components/general-components/divider-waves-bottom.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./../scss-components/divider-bottom.module.scss";
+
+function DividerBottom() {
+    return <div className={styles.divider} />;
+}
+
+export default DividerBottom;
