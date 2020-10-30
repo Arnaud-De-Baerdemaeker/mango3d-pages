3303b38be603e9d57eff5998ceaa85df785c3afe
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 17:01:42 +0100

    Add styling
    Imported styles from the SCSS file.
    Added the className to the tags.

--- pages/components/overview-page/for-all-industries-components/jewelry-ring.js
+++ pages/components/overview-page/for-all-industries-components/jewelry-ring.js
@@ -1,26 +1,29 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../../scss-components/models.module.scss";
+
 function JewelryRing() {
     return (
-        <div>
-            <figure>
+        <>
+            <figure className={styles.models__imageContainer}>
                 <img
                     src={"/lychee-slicer-jewelry-ring.jpg"}
                     title={"lychee-slicer-jewelry-ring"}
+                    className={styles.models__image}
                 />
-                <figcaption>
+                <figcaption className={styles.models__imageCaption}>
                     {"3D model :"}
                     <Link
                         href={
                             "https://fr.3dexport.com/free-3dmodel-jewellery-ring-192865.htm"
                         }>
-                        <a>{"Nirvanna"}</a>
+                        <a className={styles.models__imageLink}>{"Nirvanna"}</a>
                     </Link>
                 </figcaption>
             </figure>
-            <h3>{"Jewelry Design"}</h3>
-        </div>
+            <h3 className={styles.models__title}>{"Jewelry Design"}</h3>
+        </>
     );
 }
 
