0fdb0d1b203346b11642beb235fb9c5caebcea82
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-30 17:01:31 +0100

    Add styling
    Imported styles from the SCSS file.
    Added the className to the tags.

--- pages/components/overview-page/for-all-industries-components/dental.js
+++ pages/components/overview-page/for-all-industries-components/dental.js
@@ -1,23 +1,30 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../../scss-components/models.module.scss";
+
 function Dental() {
     return (
-        <div>
-            <figure>
+        <>
+            <figure className={styles.models__imageContainer}>
                 <img
                     src={"/dental-lychee-slicer.jpg"}
                     title={"dental-lychee-slicer"}
+                    className={styles.models__image}
                 />
-                <figcaption>
+                <figcaption className={styles.models__imageCaption}>
                     {"3D model:"}
                     <Link href={"https://3d-expert.fr/"} target={"_blank"}>
-                        <a>{"3D Expert.fr"}</a>
+                        <a className={styles.models__imageLink}>
+                            {"3D Expert.fr"}
+                        </a>
                     </Link>
                 </figcaption>
             </figure>
-            <h3>{"Dental and Orthodontics"}</h3>
-        </div>
+            <h3 className={styles.models__title}>
+                {"Dental and Orthodontics"}
+            </h3>
+        </>
     );
 }
 
