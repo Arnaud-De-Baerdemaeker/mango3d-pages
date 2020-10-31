80cfb2a36852bbf87ae20b11f124f3d1bfb2782e
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 15:44:42 +0100

    Add the className to the tags

--- pages/components/overview-page/advanced-support-management-components/parenting-bracing.js
+++ pages/components/overview-page/advanced-support-management-components/parenting-bracing.js
@@ -1,16 +1,23 @@
 import React from "react";
 
+import styles from "./../../scss-components/advanced-support-management-models.module.scss";
+
 function ParentingBracing() {
     return (
-        <div>
-            <div>
-                <h3>{"Parenting &#38; Bracing"}</h3>
-                <figure>
+        <div className={styles.models}>
+            <div className={styles.models__titleContainer}>
+                <h3 className={styles.models__title}>
+                    {"Parenting & Bracing"}
+                </h3>
+            </div>
+            <div className={styles.models__model}>
+                <figure className={styles.models__imageContainer}>
                     <img
                         src={"/feature_auto_bracing2.gif"}
                         title={"feature_auto_bracing2"}
+                        className={styles.models__image}
                     />
-                    <figcaption>
+                    <figcaption className={styles.models__imageCaption}>
                         {
                             "Automatically create connections between supports for improved strength and saving resin cost."
                         }
