c66287c62baf82525c430740d92f84ed8ea5dbb3
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 16:06:16 +0100

    Add the className to the tags

--- pages/components/overview-page/advanced-support-management-components/dynamic-raft.js
+++ pages/components/overview-page/advanced-support-management-components/dynamic-raft.js
@@ -1,13 +1,21 @@
 import React from "react";
 
+import styles from "./../../scss-components/advanced-support-management-models.module.scss";
+
 function DynamicRaft() {
     return (
-        <div>
-            <div>
-                <h3>{"Dynamic Raft"}</h3>
-                <figure>
-                    <img src={"/feature_raft.jpg"} title={"feature_raft"} />
-                    <figcaption>
+        <div className={styles.models}>
+            <div className={styles.models__titleContainer}>
+                <h3 className={styles.models__title}>{"Dynamic Raft"}</h3>
+            </div>
+            <div className={styles.models__model}>
+                <figure className={styles.models__imageContainer}>
+                    <img
+                        src={"/feature_raft.jpg"}
+                        title={"feature_raft"}
+                        className={styles.models__image}
+                    />
+                    <figcaption className={styles.models__imageCaption}>
                         {
                             "Several raft shapes that fit your model can be added to ensure a strong bed adhesion."
                         }
