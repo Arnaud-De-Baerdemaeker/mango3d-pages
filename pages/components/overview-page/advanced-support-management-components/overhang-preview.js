effe328f8a6b83a731fec637e5206e7d70911598
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 15:50:19 +0100

    Add the className to the tags

--- pages/components/overview-page/advanced-support-management-components/overhang-preview.js
+++ pages/components/overview-page/advanced-support-management-components/overhang-preview.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import styles from "./../../scss-components/advanced-support-management-models.module.scss";
+
 function OverhangPreview() {
     return (
-        <div>
-            <div>
-                <h3>{"Overhang Preview"}</h3>
-                <figure>
+        <div className={styles.models}>
+            <div className={styles.models__titleContainer}>
+                <h3 className={styles.models__title}>{"Overhang Preview"}</h3>
+            </div>
+            <div className={styles.models__model}>
+                <figure className={styles.models__imageContainer}>
                     <img
                         src={"/feature_overhang2.gif"}
                         title={"feature_overhang2"}
+                        className={styles.models__image}
                     />
-                    <figcaption>
+                    <figcaption className={styles.models__imageCaption}>
                         {
                             "Lychee shows you in real-time areas that need supports. Add supports to reduce or remove these areas."
                         }
