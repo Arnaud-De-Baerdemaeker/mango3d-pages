4a3d72def50ec9c00fcddea2ded851ad26e440d6
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 14:53:38 +0100

    Build the top divider component

--- pages/components/general-components/divider-waves-top.js
+++ pages/components/general-components/divider-waves-top.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function Divider() {
-    return <div />;
+import styles from "./../scss-components/divider-top.module.scss";
+
+function DividerTop() {
+    return <div className={styles.dividerTop} />;
 }
 
-export default Divider;
+export default DividerTop;
