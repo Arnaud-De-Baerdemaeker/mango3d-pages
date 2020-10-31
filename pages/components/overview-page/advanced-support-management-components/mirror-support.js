2baf5e331958932d1cec64febfcf6c2e54e69570
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 15:47:54 +0100

    Add the ClassName to the tags

--- pages/components/overview-page/advanced-support-management-components/mirror-support.js
+++ pages/components/overview-page/advanced-support-management-components/mirror-support.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import styles from "./../../scss-components/advanced-support-management-models.module.scss";
+
 function MirrorSupport() {
     return (
-        <div>
-            <div>
-                <h3>{"Mirror Support"}</h3>
-                <figure>
+        <div className={styles.models}>
+            <div className={styles.models__titleContainer}>
+                <h3 className={styles.models__title}>{"Mirror Support"}</h3>
+            </div>
+            <div className={styles.models__model}>
+                <figure className={styles.models__imageContainer}>
                     <img
                         src={"/feature_mirror_support.jpg"}
                         title={"feature_mirror_support"}
+                        className={styles.models__image}
                     />
-                    <figcaption>
+                    <figcaption className={styles.models__imageCaption}>
                         {
                             "Place your supports symmetrically with the help of the mirror option saving you production time."
                         }
