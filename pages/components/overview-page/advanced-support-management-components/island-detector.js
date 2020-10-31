735b77297a97e09781d3fc3125fb2b7588643673
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-10-31 15:44:25 +0100

    Add the className to the tags

--- pages/components/overview-page/advanced-support-management-components/island-detector.js
+++ pages/components/overview-page/advanced-support-management-components/island-detector.js
@@ -1,16 +1,21 @@
 import React from "react";
 
+import styles from "./../../scss-components/advanced-support-management-models.module.scss";
+
 function IslandDetector() {
     return (
-        <div>
-            <div>
-                <h3>{"Island Detector"}</h3>
-                <figure>
+        <div className={styles.models}>
+            <div className={styles.models__titleContainer}>
+                <h3 className={styles.models__title}>{"Island Detector"}</h3>
+            </div>
+            <div className={styles.models__model}>
+                <figure className={styles.models__imageContainer}>
                     <img
                         src={"/feature_island2.gif"}
                         title={"feature_island2"}
+                        className={styles.models__image}
                     />
-                    <figcaption>
+                    <figcaption className={styles.models__imageCaption}>
                         {
                             "Find easily areas that require supports with the detection of islands. Spot them in seconds."
                         }
