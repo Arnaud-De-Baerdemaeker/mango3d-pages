67d91d114ae79ab72b723615065097520c1692e5
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 17:36:25 +0100

    [wip] Build the back to top component
    Imported the CSS file.
    Imported the Font Awesome icon.
    Added the className to the <div>.
    Building the function to control the button.

--- pages/components/general-components/back-to-top.js
+++ pages/components/general-components/back-to-top.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React /*, {useState}*/ from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleUp} from "@fortawesome/free-solid-svg-icons";
+
+import styles from "./../scss-components/back-to-top.module.scss";
 
 function BackToTop() {
-    return <div />;
+    // const [isDisplayed, useIsDisplayed] = useState(false);
+    // const position = Element.scrollTop();
+
+    // const scrollOrNot = () => {
+    //     if (position > 1080)
+    //     {
+    //         return useIsDisplayed(true);
+    //     }
+    //     else
+    //     {
+    //         return isDisplayed;
+    //     }
+    // }
+
+    return (
+        <div className={styles.backToTop}>
+            <FontAwesomeIcon
+                icon={faAngleUp}
+                // onClick={scrollToTop()}
+                // className={}
+            />
+        </div>
+    );
 }
 
 export default BackToTop;
