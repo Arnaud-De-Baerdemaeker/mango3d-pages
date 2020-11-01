0d0885ae41cab28c61c17559d3a63e99b9f11291
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 13:22:23 +0100

    Style the component
    Imported the styles from the CSS module.
    Added the className to the <div>.

--- pages/components/general-components/divider-arrow.js
+++ pages/components/general-components/divider-arrow.js
@@ -1,7 +1,9 @@
 import React from "react";
 
+import styles from "./../scss-components/arrow-divider.module.scss";
+
 function ArrowDivider() {
-    return <div />;
+    return <div className={styles.arrowDivider} />;
 }
 
 export default ArrowDivider;
