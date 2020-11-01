de1f37bde3e153ebb564fcd77371c35503732b0e
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 13:05:14 +0100

    Add classNames attributes to the tags

--- pages/components/overview-page/realtime-operations.js
+++ pages/components/overview-page/realtime-operations.js
@@ -1,20 +1,29 @@
 import React from "react";
+
+import styles from "./../scss-components/realtime-operations.module.scss";
+
 import RealtimeHollowing from "./realtime-operations-components/realtime-hollowing";
 import DrainHoles from "./realtime-operations-components/drain-holes";
 import RealtimePreview from "./realtime-operations-components/realtime-preview";
 
 function RealtimeOperations() {
     return (
-        <div>
-            <div>
-                <h2>{"Realtime operations"}</h2>
-                <p>
-                    {
-                        "Because printing a model can take hours (or days!), it is crucial to anticipate the 3D printing constraints, and being sure the optimizations you are doing, like hollowing, are done the right way. Lychee Slicer provides several tools and functions that are working realtime: check the result right away and reduce the risks of printing failures."
-                    }
-                </p>
+        <div className={styles.realtimeOperations}>
+            <div className={styles.realtimeOperations__intro}>
+                <div className={styles.realtimeOperations__titleContainer}>
+                    <h2 className={styles.realtimeOperations__title}>
+                        {"Realtime operations"}
+                    </h2>
+                </div>
+                <div className={styles.realtimeOperations__textContainer}>
+                    <p className={styles.realtimeOperations__text}>
+                        {
+                            "Because printing a model can take hours (or days!), it is crucial to anticipate the 3D printing constraints, and being sure the optimizations you are doing, like hollowing, are done the right way. Lychee Slicer provides several tools and functions that are working realtime: check the result right away and reduce the risks of printing failures."
+                        }
+                    </p>
+                </div>
             </div>
-            <div>
+            <div className={styles.realtimeOperations__models}>
                 <RealtimeHollowing />
                 <DrainHoles />
                 <RealtimePreview />
