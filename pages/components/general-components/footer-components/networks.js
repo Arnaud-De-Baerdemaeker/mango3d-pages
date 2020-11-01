5c7ae040b72f58baf9aba6419b024642ad2d70d4
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 16:33:40 +0100

    Style the network component
    Imported the styles from the CSS file.
    Imported the icons from Font Awesome.
    Added the className to the tags.

--- pages/components/general-components/footer-components/networks.js
+++ pages/components/general-components/footer-components/networks.js
@@ -1,56 +1,68 @@
 import React from "react";
 import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRss} from "@fortawesome/free-solid-svg-icons";
+import {
+    faDiscord,
+    faInstagram,
+    faTwitter,
+    faFacebookF,
+    faFacebookSquare,
+    faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+import styles from "./../../scss-components/networks.module.scss";
 
 function Networks() {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className={styles.networks}>
+            <ul className={styles.networks__list}>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faDiscord} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faInstagram} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faTwitter} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faFacebookF} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faFacebookSquare} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faYoutube} />
                         </a>
                     </Link>
                 </li>
-                <li>
+                <li className={styles.networks__listItem}>
                     <Link href={"#"}>
-                        <a>
-                            <span />
+                        <a className={styles.networks__link}>
+                            <FontAwesomeIcon icon={faRss} />
                         </a>
                     </Link>
                 </li>
