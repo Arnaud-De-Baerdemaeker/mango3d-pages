c5bbbb16c8f36f38b9ee79294ac8997357cfbb58
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 14:15:28 +0100

    Style the CompatibilityList component
    Imported the CSS file.
    Added the className to the tags.

--- pages/components/overview-page/compatibility-list.js
+++ pages/components/overview-page/compatibility-list.js
@@ -2,25 +2,32 @@ import React from "react";
 import Link from "next/link";
 
 import ArrowDivider from "./../general-components/divider-arrow";
+import styles from "./../scss-components/compatibility-list.module.scss";
 
 function CompatibilityList() {
     return (
-        <div>
+        <div className={styles.compatibilityList}>
             <ArrowDivider />
-            <div>
-                <div>
-                    <h2>{"Lychee 3D Printers Compatibility list"}</h2>
+            <div className={styles.compatibilityList__intro}>
+                <div className={styles.compatibilityList__titleContainer}>
+                    <h2 className={styles.compatibilityList__title}>
+                        {"Lychee 3D Printers Compatibility list"}
+                    </h2>
                 </div>
-                <div>
-                    <p>
+                <div className={styles.compatibilityList__textContainer}>
+                    <p className={styles.compatibilityList__text}>
                         {
                             "Lychee Slicer supports a growing list of 3D Printers natively: prepare, optimize, and slice before exporting in a native format for your 3D printer. There is a high chance that your 3D printer is supported."
                         }
                     </p>
                 </div>
             </div>
-            <div>
-                <Link href={"#"}>{"List of supported 3D printers"}</Link>
+            <div className={styles.compatibilityList__linkContainer}>
+                <Link href={"#"}>
+                    <a className={styles.compatibilityList__link}>
+                        {"List of supported 3D printers"}
+                    </a>
+                </Link>
             </div>
         </div>
     );
