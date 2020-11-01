49a33c29432e058b4ea929fdb7afabf9fc388b36
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-01 15:35:16 +0100

    Style the footer component
    Imported the styles from the CSS file.
    Added the className to the tags.

--- pages/components/general-components/footer.js
+++ pages/components/general-components/footer.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../scss-components/footer.module.scss";
+
 import Networks from "./footer-components/networks";
 
 function Footer() {
     return (
-        <div>
-            <div>
-                <p>
+        <div className={styles.footer}>
+            <div className={styles.footer__titleContainer}>
+                <p className={styles.footer__title}>
                     {
-                        "We design and develop experiences that make people's life easier."
+                        '"We design and develop experiences that make people\'s life easier."'
                     }
                 </p>
             </div>
-            <div>
-                <div>
-                    <p>
+            <div className={styles.footer__intro}>
+                <div className={styles.footer__mangoCopyrightContainer}>
+                    <p className={styles.footer__mangoCopyrightText}>
                         {
                             "Mango is a company with at its core a team of developers and electronics engineers passionate about 3D printing and related technologies, who develop software and hardware solutions for individuals, professionals and manufacturers."
                         }
+                        <br />
+                        {"Mango is based in Belgium and France."}
                     </p>
                 </div>
-                <div>
-                    <p>
+                <div className={styles.footer__mangoCopyrightContainer}>
+                    <p className={styles.footer__mangoCopyrightText}>
                         {"Copyright 2020 © Mango3d."}
                         <br />
                         {
@@ -30,27 +34,33 @@ function Footer() {
                         }
                     </p>
                 </div>
-                <div>
-                    <ul>
-                        <li>
+                <div className={styles.footer__linksContainer}>
+                    <ul className={styles.footer__list}>
+                        <li className={styles.footer__listItem}>
                             <Link href={"#"}>
-                                <a>{"Contact us"}</a>
+                                <a className={styles.footer__link}>
+                                    {"Contact us"}
+                                </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className={styles.footer__listItem}>
                             <Link href={"#"}>
-                                <a>{"Privacy policy"}</a>
+                                <a className={styles.footer__link}>
+                                    {"Privacy policy"}
+                                </a>
                             </Link>
                         </li>
-                        <li>
+                        <li className={styles.footer__listItem}>
                             <Link href={"#"}>
-                                <a>{"Terms and Conditions"}</a>
+                                <a className={styles.footer__link}>
+                                    {"Terms and Conditions"}
+                                </a>
                             </Link>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div>
+            <div className={styles.footer__networks}>
                 <Networks />
             </div>
         </div>
