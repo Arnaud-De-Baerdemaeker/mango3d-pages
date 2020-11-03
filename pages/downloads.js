6eb0f0d7bb1b88028f717e7230e2c83c54b22993
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-03 11:41:02 +0100

    Build the function to get data from JSON

--- pages/downloads.js
+++ pages/downloads.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/general-components/header";
 import Intro from "./components/downloads-page/intro";
+import Versions from "./components/downloads-page/versions";
+import Windows from "./components/downloads-page/windows";
 import Iframe from "./components/downloads-page/iframe";
 import Footer from "./components/general-components/footer";
 
-function Downloads() {
+function Downloads({data}) {
     return (
         <>
             <style jsx global>
@@ -14,10 +16,24 @@ function Downloads() {
             </style>
             <Header />
             <Intro />
+            <Versions data={data} />
+            <Windows />
             <Iframe />
             <Footer />
         </>
     );
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(
+        "https://api.mango3d.io/applications/a8ee1146-8d03-4b69-8a67-59009a3f9ee7",
+    );
+    const data = await res.json();
+    return {
+        props: {
+            data,
+        },
+    };
 }
 
 export default Downloads;
