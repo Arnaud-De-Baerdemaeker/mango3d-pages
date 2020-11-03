93cf0cce7dc86ab6319d8552aba6de1db4a98f3b
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-03 11:38:16 +0100

    Style the Versions component
    Added the className to the tags.

--- pages/components/downloads-page/versions.js
+++ pages/components/downloads-page/versions.js
@@ -1,81 +1,109 @@
 import React from "react";
 import Link from "next/link";
 
+import styles from "./../scss-components/versions.module.scss";
+
 function Versions({data}) {
     const name = data.name.en;
     const nameNoSpace = name.split(" ").join("");
 
     return (
-        <div>
-            <div>
-                <div>{`Stable - Current version: ${data.url.latest}`}</div>
-                <ul>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}+Setup+${data.url.latest}.exe`}>
-                            <a>{"Windows 64-bit"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.dmg`}>
-                            <a>{"Mac OS"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}_${data.url.latest}_amd64.deb`}>
-                            <a>{"Linux Deb"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.tar.gz`}>
-                            <a>{"Linux tar.gz"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.AppImage`}>
-                            <a>{"Linux App Img"}</a>
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <div>{`Public Beta - Current version: ${data.url.beta}`}</div>
-                <ul>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}+Setup+${data.url.beta}.exe`}>
-                            <a>{"Windows 64-bit"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.dmg`}>
-                            <a>{"Mac OS"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}_${data.url.beta}_amd64.deb`}>
-                            <a>{"Linux Deb"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.tar.gz`}>
-                            <a>{"Linux tar.gz"}</a>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.AppImage`}>
-                            <a>{"Linux App Img"}</a>
-                        </Link>
-                    </li>
-                </ul>
+        <div className={styles.versions}>
+            <div className={styles.versions__Container}>
+                <div className={styles.versions__stableContainer}>
+                    <div className={styles.versions__stableTitle}>
+                        {`Stable - Current version: ${data.url.latest}`}
+                    </div>
+                    <ul className={styles.versions__stableList}>
+                        <li className={styles.versions__stableListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}+Setup+${data.url.latest}.exe`}>
+                                <a className={styles.versions__stableLink}>
+                                    {"Windows 64-bit"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__stableListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.dmg`}>
+                                <a className={styles.versions__stableLink}>
+                                    {"Mac OS"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__stableListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}_${data.url.latest}_amd64.deb`}>
+                                <a className={styles.versions__stableLink}>
+                                    {"Linux Deb"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__stableListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.tar.gz`}>
+                                <a className={styles.versions__stableLink}>
+                                    {"Linux tar.gz"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__stableListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.latest}.AppImage`}>
+                                <a className={styles.versions__stableLink}>
+                                    {"Linux App Img"}
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.versions__betaContainer}>
+                    <div className={styles.versions__betaTitle}>
+                        {`Public Beta - Current version: ${data.url.beta}`}
+                    </div>
+                    <ul className={styles.versions__betaList}>
+                        <li className={styles.versions__betaListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}+Setup+${data.url.beta}.exe`}>
+                                <a className={styles.versions__betaLink}>
+                                    {"Windows 64-bit"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__betaListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.dmg`}>
+                                <a className={styles.versions__betaLink}>
+                                    {"Mac OS"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__betaListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}_${data.url.beta}_amd64.deb`}>
+                                <a className={styles.versions__betaLink}>
+                                    {"Linux Deb"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__betaListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.tar.gz`}>
+                                <a className={styles.versions__betaLink}>
+                                    {"Linux tar.gz"}
+                                </a>
+                            </Link>
+                        </li>
+                        <li className={styles.versions__betaListItem}>
+                            <Link
+                                href={`https://mango-lychee.s3.eu-west-3.amazonaws.com/${nameNoSpace}-${data.url.beta}.AppImage`}>
+                                <a className={styles.versions__betaLink}>
+                                    {"Linux App Img"}
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
