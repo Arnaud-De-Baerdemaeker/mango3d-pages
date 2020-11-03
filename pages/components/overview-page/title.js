3b55f9981652440053890acc81bd8a8038c91cf2
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-03 15:32:25 +0100

    Add control to display icons
    Built a function to change the state on hovering the button.
    Built a function to make the arrow appear on the button.
    Built a function to change the state on leaving the button.
    Added an icon from Font Awesome.

--- pages/components/overview-page/title.js
+++ pages/components/overview-page/title.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowAltCircleRight} from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./../scss-components/title.module.scss";
 
 function Title() {
+    const [isButton1Hovered, setIsButton1Hovered] = useState(false);
+    const [isButton2Hovered, setIsButton2Hovered] = useState(false);
+
+    const onButton1 = () => {
+        setIsButton1Hovered(true);
+    };
+
+    const onButton2 = () => {
+        setIsButton2Hovered(true);
+    };
+
+    const leaveButton1 = () => {
+        setIsButton1Hovered(false);
+    };
+
+    const leaveButton2 = () => {
+        setIsButton2Hovered(false);
+    };
+
+    const handleArrow1Display = () => {
+        if (isButton1Hovered === true) {
+            return styles.title__arrow1;
+        }
+
+        return styles.title__arrow1Hidden;
+    };
+
+    const handleArrow2Display = () => {
+        if (isButton2Hovered === true) {
+            return styles.title__arrow2;
+        }
+
+        return styles.title__arrow2Hidden;
+    };
+
     return (
         <div className={styles.title__container}>
             <div className={styles.title__subContainer}>
@@ -24,17 +61,31 @@ function Title() {
                 </div>
             </div>
             <div className={styles.title__links}>
-                <div className={styles.title__button}>
+                <div
+                    className={styles.title__button}
+                    onMouseOver={onButton1}
+                    onMouseLeave={leaveButton1}>
                     <Link href={"/downloads"}>
                         <a className={styles.title__link}>
                             {"Get Lychee Slicer 3 Free Edition"}
+                            <FontAwesomeIcon
+                                icon={faArrowAltCircleRight}
+                                className={handleArrow1Display()}
+                            />
                         </a>
                     </Link>
                 </div>
-                <div className={styles.title__button}>
+                <div
+                    className={styles.title__button}
+                    onMouseOver={onButton2}
+                    onMouseLeave={leaveButton2}>
                     <Link href={"#"}>
                         <a className={styles.title__link}>
                             {"Compare versions"}
+                            <FontAwesomeIcon
+                                icon={faArrowAltCircleRight}
+                                className={handleArrow2Display()}
+                            />
                         </a>
                     </Link>
                 </div>
