b95e553d1063024b55d73a2b40c894ad7061b94b
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-04 14:13:14 +0100

    Add a new function to set a "back to top" option
    Removed the unused useState and useEffect imports.

--- pages/components/general-components/back-to-top.js
+++ pages/components/general-components/back-to-top.js
@@ -1,31 +1,25 @@
-import React /*, {useState}*/ from "react";
+import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAngleUp} from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./../scss-components/back-to-top.module.scss";
 
-function BackToTop() {
-    // const [isDisplayed, useIsDisplayed] = useState(false);
-    // const position = Element.scrollTop();
+function BackToTop({scrollValue}) {
+    const buttonIsDisplayed = () => {
+        if (scrollValue >= 200) {
+            return styles.backToTop;
+        }
 
-    // const scrollOrNot = () => {
-    //     if (position > 1080)
-    //     {
-    //         return useIsDisplayed(true);
-    //     }
-    //     else
-    //     {
-    //         return isDisplayed;
-    //     }
-    // }
+        return styles.backToTopHidden;
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo(0, 0);
+    };
 
     return (
-        <div className={styles.backToTop}>
-            <FontAwesomeIcon
-                icon={faAngleUp}
-                // onClick={scrollToTop()}
-                // className={}
-            />
+        <div onClick={scrollToTop} className={buttonIsDisplayed()}>
+            <FontAwesomeIcon icon={faAngleUp} />
         </div>
     );
 }
