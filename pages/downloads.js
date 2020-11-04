8be1d8e63e58d49246aa618e597e0a648ef543d2
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-11-04 16:57:49 +0100

    Finalize the code
    Imported the useState and useEffect.
    Build a useEffect to get the value of the scroll.
    Imported the BackToTop component.
    Added comments.

--- pages/downloads.js
+++ pages/downloads.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./components/general-components/header";
 import Intro from "./components/downloads-page/intro";
 import Versions from "./components/downloads-page/versions";
 import Windows from "./components/downloads-page/windows";
 import Iframe from "./components/downloads-page/iframe";
 import Footer from "./components/general-components/footer";
+import BackToTop from "./components/general-components/back-to-top";
 
 function Downloads({data}) {
+    const [scrollValue, setScrollValue] = useState();
+
+    // Get the value when the user scrolls
+    useEffect(() => {
+        const getScrollValue = () => {
+            setScrollValue(window.pageYOffset);
+        };
+
+        window.onscroll = () => {
+            getScrollValue();
+        };
+    }, []);
+
     return (
         <>
             <style jsx global>
                 {
-                    "*{ box-sizing: border-box; } body{ margin: 0px; color: rgb(102, 102, 102); -webkit-font-smoothing: antialiased; font-weight: 500; }"
+                    "*{ box-sizing: border-box; scroll-behavior: smooth; } body{ margin: 0px; color: rgb(102, 102, 102); -webkit-font-smoothing: antialiased; font-weight: 500; }"
                 }
             </style>
             <Header />
@@ -20,10 +34,12 @@ function Downloads({data}) {
             <Windows />
             <Iframe />
             <Footer />
+            <BackToTop scrollValue={scrollValue} />
         </>
     );
 }
 
+// Fetch data using SSR
 export async function getServerSideProps() {
     const res = await fetch(
         "https://api.mango3d.io/applications/a8ee1146-8d03-4b69-8a67-59009a3f9ee7",
